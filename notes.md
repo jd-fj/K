6d18589af25b42c679ef381e6bb8c6252d360394
tdelph <tdelph@newrelic.com>
2023-05-23 21:34:57 -0700

    about/home page routed links

--- notes.md
+++ notes.md
@@ -3,3 +3,16 @@ Photography page
 reiki page
 mindfulness page
 aboot
+
+
+
+Home page
+  - a picture of anything
+Photography page
+  - a picture of anything
+reiki page
+  - the word "reiki"
+mindfulness page
+  - a color (probably yellow oragne or green or brown)
+aboot
+  - the word Kalee
--- src/app.tsx
+++ src/app.tsx
@@ -1,56 +1,21 @@
-import { useState } from 'preact/hooks'
-import smallFrown from './assets/smallFrown.svg'
-import tammy from './assets/tammy.svg' // this is the normal svg
-import Tammy from './assets/Tammy' // this is the component svg
 import './app.css'
 
-
-type LogoLinkProps = {
-  href: string;
-  imgSrc: string;
-  alt: string;
-  className: string;
-};
-
-function LogoLink({ href, imgSrc, alt, className }: LogoLinkProps) {
-  return (
-    <a href={href} target="_blank">
-      <img src={imgSrc} className={className} alt={alt} />
-    </a>
-  );
-}
+import Router from 'preact-router';
+import { Link } from 'preact-router/match';
+import Home from './components/home'
+import About from './components/about'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="flex justify-center items-center">
-        <LogoLink
-          href="https://vitejs.dev"
-          imgSrc={tammy}
-          className="logo tammy"
-          alt="tammy logo"
-        />
-        <LogoLink
-          href="https://preactjs.com"
-          imgSrc={smallFrown}
-          className="logo frowny"
-          alt="frowny logo"
-        />
-      </div>
-      <h1>Vite + Preact</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          <Tammy /> {count}
-        </button>
-        <p>
-          Editing <code>src/app.tsx</code> and saving shows Vite's HMR (hot module reload)
-        </p>
-      </div>
-      <p className="text-red-600">
-        THIS TEXT IS READ BECAUSE TAILWIND
-      </p>
+      <nav>
+        <Link activeClassName="active" href="/">Home</Link>
+        <Link activeClassName="active" href="/about">About</Link>
+      </nav>
+      <Router>
+        <Home path="/" />
+        <About path="/about" />
+      </Router>
     </>
   )
 }
