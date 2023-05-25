2a8dd0ab9533239421df8667424376d3559e4847
tdelph <tdelph@newrelic.com>
2023-05-25 11:10:26 -0700

    make it gh-pagable

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+// main.tsx
+
 import { render } from 'preact'
 import { App } from './app.tsx'
 import './index.css'
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+// in vite-env.d.ts
+interface ImportMeta {
+  env: {
+    NODE_ENV?: 'development' | 'production'
+    MODE?: 'development' | 'production'
+    [key: string]: string | undefined
+  }
+}
