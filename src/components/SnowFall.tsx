7023b0543447e222951d77e50ff725b3dd2f2065
tdelph <tdelph@newrelic.com>
2023-05-31 15:58:07 -0700

    start snow higher on screen

--- src/components/SnowFall.tsx
+++ src/components/SnowFall.tsx
@@ -1,6 +1,4 @@
-// SnowFall.tsx
-// import { FunctionalComponent } from 'preact';
-import { useEffect, useState } from 'preact/hooks';
+import { useEffect, useState, useRef } from 'preact/hooks';
 import KitchyFlower from '../assets/KitchyFlower';
 import './snowStyles.css';
 
@@ -12,28 +10,41 @@ type Flower = {
 
 const SnowFall = () => {
     const [flowers, setFlowers] = useState<Flower[]>([]);
-    const flowerCount = 50;
+    const flowersRef = useRef<Flower[]>([]);
+    const flowerCount = 100;
 
     useEffect(() => {
         const newFlowers = [];
         for (let i = 0; i < flowerCount; i++) {
             const x = Math.random() * window.innerWidth;
-            const y = Math.random() * window.innerHeight;
-            newFlowers.push({ x, y, speed: Math.random() * .5 });
+            const y = -100;  // Start the flowers from the top of the screen
+            newFlowers.push({ x, y, speed: Math.random() + .95  * .75 });
         }
         setFlowers(newFlowers);
+        flowersRef.current = newFlowers;
     }, []);
 
     useEffect(() => {
         let animationFrameId: number | null = null;
         const moveFlowers = () => {
-            setFlowers(flowers =>
-                flowers.map(flower => ({
-                    ...flower,
-                    y: (flower.y + flower.speed) % window.innerHeight,
-                }))
-            );
-            animationFrameId = requestAnimationFrame(moveFlowers);
+            const newFlowers = flowersRef.current.map(flower => {
+                if (flower.y + flower.speed >= window.innerHeight) {
+                    // If the flower has reached the bottom, return it without updating y
+                    return flower;
+                }
+                return { ...flower, y: flower.y + flower.speed };
+            });
+
+            // Filter out flowers that have reached the bottom of the screen
+            const remainingFlowers = newFlowers.filter(flower => flower.y + flower.speed < window.innerHeight);
+            
+            setFlowers(remainingFlowers);
+            flowersRef.current = remainingFlowers;
+
+            // Stop animation if all flowers have reached the bottom
+            if (remainingFlowers.length > 0) {
+                animationFrameId = requestAnimationFrame(moveFlowers);
+            }
         };
         moveFlowers();
         return () => {
@@ -45,8 +56,9 @@ const SnowFall = () => {
 
     return (
         <div className="snowfall z-0">
-            {flowers.map(flower => (
+            {flowers.map((flower, index) => (
                 <div
+                    key={index}  // Don't forget to provide keys when mapping over array in React/Preact
                     class="flower"
                     style={{
                         position: 'absolute',
