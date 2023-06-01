55bfe1b3fe85a5d4402b58ffcf8059bcd4bc643f
tdelph <tdelph@newrelic.com>
2023-05-31 17:54:13 -0700

    make a photo "grid"

--- src/components/Film.tsx
+++ src/components/Film.tsx
@@ -6,7 +6,9 @@ const Film: FunctionComponent = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1>Film</h1>
-      <Mush />
+      <div className="grid grid-cols-3 gap-8 justify-items-center">
+        {Array(9).fill(<Mush />)}
+      </div>
     </div>
   );
 }
