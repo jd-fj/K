87a5acc805074af8872fe869945c2bc5cac29720
tdelph <tdelph@newrelic.com>
2023-05-31 18:27:09 -0700

    flex-col to reiki page

--- src/components/Reiki.tsx
+++ src/components/Reiki.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from 'preact';
 
 const Reiki: FunctionComponent = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1>Reiki</h1>
       <p>This is the Reiki page</p>
     </div>
