e3a6f632444503813268680aec967864d231d15e
tdelph <tdelph@newrelic.com>
2023-06-08 13:14:21 -0700

    make palette uniform long rectangles w/padding

--- src/components/Palette.tsx
+++ src/components/Palette.tsx
@@ -2,40 +2,44 @@ import { useState } from 'preact/hooks';
 import { FunctionComponent } from 'preact';
 
 const Palette: FunctionComponent = () => {
-    const initialColors = Array(8).fill('000000'); // initial color is black for all squares
-    const [colors, setColors] = useState<string[]>(initialColors);
+  const initialColors = Array(8).fill('000000');
+  const [colors, setColors] = useState<string[]>(initialColors);
 
-    const handleChange = (index: number) => (e: any) => {
-        const newColors = [...colors];
-        newColors[index] = e.target.value;
-        setColors(newColors);
+  // change the color of the input that was changed
+  const handleChange = (index: number) => (e: any) => {
+    console.log("INdex? ", index)
+    console.log("Colors? ", colors)
+    const newColors = [...colors];
+    newColors[index] = e.target.value;
+    setColors(newColors);
+  }
+
+  // prevent form submission (maybe not necessary)
+  const handleKeyPress = (e: any) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
     }
+  }
 
-    const handleKeyPress = (e: any) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-        }
-    }
-
-    return (
-        <div className="flex items-end justify-center h-screen">
-            {colors.map((color, index) => (
-                <div key={index} className="">
-                    <input 
-                        type="text"
-                        value={color}
-                        onInput={handleChange(index)}
-                        onKeyPress={handleKeyPress}
-                        placeholder="Enter hex color..."
-                        className="bg-white-50"
-                    />
-                    <svg width="100" height="500">
-                        <rect width="100" height="500" fill={`#${color}`} />
-                    </svg>
-                </div>
-            ))}
+  return (
+    <div className="flex items-end justify-center h-screen space-x-4">
+      {colors.map((color, index) => (
+        <div key={index} className="">
+          <input
+            type="text"
+            value={color}
+            onInput={handleChange(index)}
+            onKeyPress={handleKeyPress}
+            placeholder="Enter hex color..."
+            className="bg-white-50 w-28"
+          />
+          <svg width="7rem" height="500">
+            <rect width="7rem" height="500" fill={`#${color}`} />
+          </svg>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default Palette;
