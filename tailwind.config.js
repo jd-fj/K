a2513037c89f22ecd8f2ca4ceba71014e22f60a6
tdelph <tdelph@newrelic.com>
2023-06-09 22:06:45 -0700

    add screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    // "./index.html",
+    "./main.tsx",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': {
-        50: '#FFBF7F',
-        100: '#00FF15',
-      },
+      // transparent: 'transparent',
+      // current: 'currentColor',
+      // 'white': {
+      //   50: '#FFBF7F',
+      //   100: '#00FF15',
+      // },
+    },
+    screens: {
+      'xs': '250px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',    
     },
     extend: {},
   },
