4c806855fec399bb80aed2b8c71014c6d55b713d
tdelph <tdelph@newrelic.com>
2023-06-28 20:49:51 -0700

    rearrange

--- src/components/Home/YellowFlower.tsx
+++ src/components/Home/YellowFlower.tsx
@@ -1,20 +1,18 @@
 // YellowFlower.tsx
 import { FunctionalComponent } from 'preact';
+import { useFairyDustCursor } from '../../cursor/useFairyDustCursor';
 import SnowFall from './SnowFall';
-// import Cursor from './cursor';
-import { useFairyDustCursor } from '../../hooks/useFairyDustCursor';
 
 const YellowFlower: FunctionalComponent = () => {
-  useFairyDustCursor({ colors: [
+  useFairyDustCursor({
+     colors: [
     '#FFEA00', '#ffef45', '#ffee31', '#fff04c', '#ffef44', '#fff371', '#ffee00',
     '#fef242', '#fffab1', '#fff891', '#ffe227', '#ffe851', '#ffee7e', '#fdf4b8',
     '#fff29f', '#fbe987', '#fff19f', '#f7e05f', '#fae35d', '#fbe35d', '#fdea7c',
     '#f6e063', '#ffe54f'
-  ]
-});
+  ]});
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      {/* <Cursor /> */}
       <SnowFall />
     </div>
   );
