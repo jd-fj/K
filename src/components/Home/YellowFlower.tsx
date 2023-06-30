818eb9ed772a53abec6752d8132b377f0780939b
Gelf Wilikus <you@example.com>
2023-06-29 23:38:36 -0700

    make dust one color

--- src/components/Home/YellowFlower.tsx
+++ src/components/Home/YellowFlower.tsx
@@ -6,13 +6,10 @@ import SnowFall from './SnowFall';
 const YellowFlower: FunctionalComponent = () => {
   useFairyDustCursor({
      colors: [
-    '#FFEA00', '#ffef45', '#ffee31', '#fff04c', '#ffef44', '#fff371', '#ffee00',
-    '#fef242', '#fffab1', '#fff891', '#ffe227', '#ffe851', '#ffee7e', '#fdf4b8',
-    '#fff29f', '#fbe987', '#fff19f', '#f7e05f', '#fae35d', '#fbe35d', '#fdea7c',
-    '#f6e063', '#ffe54f'
+    '#d4aa20', 
   ]});
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen z-30">
       <SnowFall />
     </div>
   );
