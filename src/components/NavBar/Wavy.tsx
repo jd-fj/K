bad270a20955b1d2ffc032c5a2ed53f16c6b98d7
Gelf Wilikus <you@example.com>
2023-07-01 00:24:20 -0700

    save weird lava-lamp effect

--- src/components/NavBar/Wavy.tsx
+++ src/components/NavBar/Wavy.tsx
@@ -1,9 +1,34 @@
 // Wavy.tsx
+import {useEffect, useRef} from "preact/hooks"
 import { FunctionalComponent } from "preact";
+import { makeNoise2D } from 'open-simplex-noise';
 
 const Wavy: FunctionalComponent = () => {
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    if (!svgRef.current) return;
+    const svg = svgRef.current;
+    const noise2D = makeNoise2D(Date.now());
+    const path = svg.querySelector('path');
+    if (!path) return;
+
+    const update = () => {
+      let d = "";
+      for (let x = 0; x < 500; x++) {
+        const y = 100 + noise2D(x * 0.01, Date.now() * 0.0001) * 50;
+        d += (x === 0 ? "M" : " L") + `${x},${y}`;
+      }
+      d += " L500,150 L0,150";
+      path.setAttribute("d", d);
+      requestAnimationFrame(update);
+    };
+    update();
+  }, []);
+
   return (
     <svg
+      ref={svgRef}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 500 150"
       height="170"
