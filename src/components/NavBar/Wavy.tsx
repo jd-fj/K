224f679e4e69946f076bcf294dc6829cc2a57ec1
Gelf Wilikus <you@example.com>
2023-07-01 00:55:43 -0700

    perfect wavy header

--- src/components/NavBar/Wavy.tsx
+++ src/components/NavBar/Wavy.tsx
@@ -16,10 +16,10 @@ const Wavy: FunctionalComponent = () => {
     const update = () => {
       let d = "";
       for (let x = 0; x < 500; x++) {
-        const y = 100 + noise2D(x * 0.01, Date.now() * 0.0001) * 50;
+        const y = 200 + noise2D(x * 0.01, Date.now() * 0.0001) * 25;  // Adjust amplitude here
         d += (x === 0 ? "M" : " L") + `${x},${y}`;
       }
-      d += " L500,150 L0,150";
+      d += " L500,300 L0,300";  // Update line here
       path.setAttribute("d", d);
       requestAnimationFrame(update);
     };
@@ -30,14 +30,14 @@ const Wavy: FunctionalComponent = () => {
     <svg
       ref={svgRef}
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 500 150"
-      height="170"
+      viewBox="0 0 500 300" // Update viewBox here
+      height="520" // Increase height here
       width="100%"
       preserveAspectRatio="none"
-      style={{fill: '#d47a20'}}
+      style={{fill: '#d47a20', transform: 'scale(-1)'}}
       className="z-0"
     >
-      <path d="M0,110 C83,165 93,70 166,110 C290,150 249,50 332,100 C415,150 415,60 550,110 L500,0 L0,0 Z"></path>
+      <path d="M0,200 C83,265 3,170 166,200 C290,250 249,150 332,200 C415,250 415,160 550,200 L500,0 L0,0 Z"/>
     </svg>
   );
 };
