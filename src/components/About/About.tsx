b2c99514b79ac171e975c94b82c665bd0014c29a
Gelf Wilikus <you@example.com>
2023-07-06 02:01:03 -0700

    try unsmoosh

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,7 +4,7 @@ import about from "../../assets/about.jpg";
 
 const About: FunctionComponent = () => {
   return (
-    <div id="parentDiv" className="flex flex-col sm:flex-row items-start justify-center mt-20 mx-10">
+    <div id="parentDiv" className="flex flex-col sm:flex-row items-start justify-center mt-20">
       <div
         id="aboutText"
         className="flex-1 text-xs sm:text-sm md:text-md lg:text-lg mx-10"
@@ -51,7 +51,7 @@ const About: FunctionComponent = () => {
         <img
           src={about}
           alt="standing and looking cute"
-          className="h-160 h-full"
+          className=""
         />
       </div>
     </div>
