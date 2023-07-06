f1ece07936c9a4a876f1e66a57dc115517172a30
Gelf Wilikus <you@example.com>
2023-07-05 22:06:25 -0700

    try margin bottom

--- src/components/Reiki.tsx
+++ src/components/Reiki.tsx
@@ -5,7 +5,7 @@ const Reiki: FunctionComponent = () => {
   return (
     <>
    {/* <div className="flex flex-col items-center justify-center"> */}
-   <div className="items-center justify-center lg:text-3xl sm:text-xl text-brown-200 mx-20 mb-80 ">
+   <div className="items-center justify-center lg:text-3xl sm:text-xl text-brown-200 mx-20 mb-60 ">
       <h1 className="lg:text-5xl sm:text-2xl">What is Reiki?</h1>
       <p className="text-brown-300">Reiki is a hands-on healing practice using qi (or "chi") to promote the body's natural ability to heal itself.</p>
    </div>
