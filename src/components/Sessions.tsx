c99d95011a8ca2de50784cc60096821482c0aeff
tdelph <tdelph@newrelic.com>
2023-08-04 16:08:13 -0700

    fix top margin

--- src/components/Sessions.tsx
+++ src/components/Sessions.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from "preact";
 
 const Sessions: FunctionComponent = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-xl mx-5 lg:mx-0 mt-[8em] text-center">
+    <div className="flex flex-col items-center justify-center text-xl mx-5 lg:mx-0 sm:mt-[8em] mt-[5em] text-center">
       <p className=" my-2">Sessions are 60 minutes long with at least 40 minutes of table time with hands on healing.</p>
       <p className=" my-2">Comfortable clothing suggested, you will not need to undress.</p>
       <p className=" my-2">We can play music or work in silence, up to you {":)"}</p>
