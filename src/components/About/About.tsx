f73177d9548f451aec123dac0239c8de2eb1e3f8
jd-fj <61722743+jd-fj@users.noreply.github.com>
2023-08-16 00:00:51 -0700

    fix merge conflicts

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,19 +1,11 @@
 // About.tsx
 import { FunctionComponent } from "preact";
-// import about from "src/assets/about.jpg";
+
 
 const About: FunctionComponent = () => {
-
   return (
     <div id="parentDiv" className="flex flex-col-reverse sm:flex-row mt-20 ">
-<<<<<<< Updated upstream
       <div id="aboutText" className="flex-1 text-xl mx-5 sm:mx-10 text-center">
-=======
-      <div
-        id="aboutText"
-        className="flex-[2.5] text-xl mx-5 sm:mx-10 text-center"
-      >
->>>>>>> Stashed changes
         <p className="mb-5">A little about me ~</p>
 
         <p className="mb-5">
@@ -40,35 +32,22 @@ const About: FunctionComponent = () => {
         <p className="mb-5">Crystal Connection</p>
 
         <p className="mb-5">
-<<<<<<< Updated upstream
-          Thank you for visiting ❤︎ If it feels right for you, please reach
-          out.
-=======
-          Thank you for visiting ❤︎{`<3`} If it feels right for you, please
-          reach out.
->>>>>>> Stashed changes
+          Thank you for visiting ❤︎ If it feels right for you, please reach out.
         </p>
 
         <p className="">Namaste</p>
         <p className="sm:mb-5">Kalee</p>
       </div>
 
-<<<<<<< Updated upstream
-      <div id="aboutImage" className="flex justify-center  items-start flex-1 mx-5 sm:mx-20 mb-5 mt-5 sm:mt-0">
-  <img
-    src={about}
-    alt="standing and looking cute"
-    className=" object-contain"
-  />
-</div>
-=======
       <div
         id="aboutImage"
-        className="flex justify-center  items-start flex-1 mx-5 sm:mx-20 mb-5"
+        className="flex justify-center  items-start flex-1 mx-5 sm:mx-20 mb-5 mt-5 sm:mt-0"
       >
-        <img src="/src/assets/about.jpg" alt="standing and looking cute" className=" object-contain" />
->>>>>>> Stashed changes
-
+        <img
+          src="/src/assets/about.jpg"
+          alt="standing and looking cute"
+          className=" object-contain"
+        />
       </div>
     </div>
   );
