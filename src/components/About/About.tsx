372e78c3de45ecea3b12bd7af7e77335fd566f1e
jd-fj <61722743+jd-fj@users.noreply.github.com>
2023-09-05 23:12:57 -0700

    fix colors and copy

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,33 +6,31 @@ const About: FunctionComponent = () => {
   return (
     <div id="parentDiv" className="flex flex-col-reverse md:flex-row mt-20 gap-[6%] ">
       <div id="aboutText" className="flex-1  text-xl md:ml-[15%] text-center md:mx-0 mx-10">
-        <p className="mb-5 -mt-4">A little about me ~</p>
+        <h1 className="mb-5 -mt-4 text-2xl">A little about me ~</h1>
 
         <p className="mb-5">
-          8 years ago I opened my path to yoga and eventually joined a Yoga
-          Teacher Training that heavily focused on breathwork and meditation. My
-          teachers welcomed me with open hearts and for that and all the wisdom
-          they shared, I am forever grateful.
+          9 years ago I opened my path to yoga and joined a Yoga Teacher Training that heavily focused on breathwork, meditation, and tuning into the body. My teachers welcomed me with open hearts and for that and all the wisdom they shared, I am forever grateful.
         </p>
 
         <p className="mb-5">
-          My interest in spirituality and alternative healing continued to grow
-          and led me to be curious about the healing power of love and energy.
-          As a Reiki recipient and Practinioner, I am truly *blissed* to be able
-          to share this experience with you all.
+          My interest in spirituality and alternative healing continued to grow and led me to want to learn more, practice healthy ways of living, and turn inward. Throughout my journey, I explored many mindfulness tools to find what worked best for my Self Practice, which include ~
         </p>
 
-        <p className="mb-5">My other knowledge/interests:</p>
+        {/* <p className="mb-5">My other knowledge/interests:</p> */}
 
-        <p className="-mb-1">Chakra System</p>
-        <p className="-mb-1">Buddhism</p>
+        <p className="-mb-1">Reiki</p>
+        <p className="-mb-1">The Chakra System</p>
         <p className="-mb-1">Hatha Yoga</p>
+        <p className="-mb-1">Buddhism</p>
+        <p className="-mb-1">Breathwork</p>
+        <p className="-mb-1">Visual Meditation</p>
         <p className="-mb-1">Internal Family System</p>
         <p className="-mb-1">Nonviolent Communication</p>
-        <p className="mb-5">Crystal Connection</p>
+        <p className="-mb-1">Crystal Connection</p>
+        <p className="mb-5">Pressure Point Massage</p>
 
         <p className="mb-5">
-          Thank you for visiting ❤︎ If it feels right for you, please reach out.
+          Appreciate you visiting ❤︎
         </p>
 
         <p className="">Namaste</p>
--- src/components/Sessions.tsx
+++ src/components/Sessions.tsx
@@ -5,14 +5,14 @@ const Sessions: FunctionComponent = () => {
   return (
     <div className="mx-[15%]">
     <div className="flex flex-col items-center justify-center text-xl sm:mt-[6em] mt-[5em] text-center">
-      <p className="text-2xl"> 60 minute Reiki Sessions $90</p> 
-      <p className="text-2xl mb-6"> Mindfulness Consulting Included</p> 
+      <h1 className="text-2xl"> 60 minute Reiki Sessions $90</h1> 
+      <h1 className="text-2xl mb-6"> Mindfulness Consulting Included</h1> 
       <p className="text-[#974] my-2">At the moment, I am taking appointments on Sundays and Monday evenings.</p>
       <p className="text-[#974] my-2">My studio is out of my home in the Renton Highlands, I have made it a very cozy and peaceful space to share.</p>
       <p className="text-[#974] my-2">I recommend comfortable, non bulky clothing but anything will do. You will not need to undress.</p>
 
 
-      <p className="text-2xl my-6  ">~What to Expect~</p> 
+      <h1 className="text-2xl my-6  ">~What to Expect~</h1> 
       <p className="text-[#974] my-2">Sessions will start with a general check in, this can be quick or extensive. We will use our time depending on what is needed.</p>
       <p className="text-[#974] my-2">During Table Time I let the energy guide me, scanning the Chakras and flowing energy to those that are in need of attention. I focus on ailments in all planes; mental, emotional, physical, spiritual and will bring in Crystals and Vibrational Sound to enhance healing if it feels right. This time will generally be very relaxing and mind clearing.</p>
       <p className="text-[#974] my-2">We will end the session with another check in. I will let you know if I felt a strong pull in specific Chakras and can recommend Mindfulness tips on how to continue your healing outside of sessions. Mending inner and outer pain is a lifelong journey and centered around consistency so any other way I can assist, I will try my best to share.</p>
@@ -20,7 +20,7 @@ const Sessions: FunctionComponent = () => {
       <p className="text-[#974] my-2">I am looking forward to connecting {":)"}</p>
 
 
-      <p className="text-2xl my-6 ">~CONTACT~</p> 
+      <h1 className="text-2xl my-6 ">~CONTACT~</h1> 
       <p className="text-[#974] my-">Text if you have my number.</p>
       <p className="text-[#974] my-">yellowflowerenergy@gmail.com</p>
       <p className="text-[#974] mb-20">@yellowflowerenergy</p>
