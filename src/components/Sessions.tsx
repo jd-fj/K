0ea2c9105eab37bbb11930d91872f63aaf9d6ee8
jd-fj <61722743+jd-fj@users.noreply.github.com>
2023-09-05 22:52:05 -0700

    session copy update

--- src/components/Sessions.tsx
+++ src/components/Sessions.tsx
@@ -3,14 +3,29 @@ import { FunctionComponent } from "preact";
 
 const Sessions: FunctionComponent = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-xl mx-5 lg:mx-0 sm:mt-[8em] mt-[5em] text-center">
-      <p className=" my-2">Sessions are 60 minutes long with at least 40 minutes of table time with hands on healing.</p>
-      <p className=" my-2">Comfortable clothing suggested, you will not need to undress.</p>
-      <p className=" my-2">We can play music or work in silence, up to you {":)"}</p>
-      <p className=" my-2">Cost for 60 minute appt $80</p>
-      <p className=" my-2">Cash or Venmo accepted.</p>
-      <p className=" my-2">I am located in the and am accepting new clients on Sundays and Monday & Friday evenings.</p>
-      <p className=" my-2">Please reach out if you are interested in learning more and want to schedule an appointment.</p>
+    <div className="mx-[15%]">
+    <div className="flex flex-col items-center justify-center text-xl sm:mt-[6em] mt-[5em] text-center">
+      <p className="text-2xl"> 60 minute Reiki Sessions $90</p> 
+      <p className="text-2xl mb-6"> Mindfulness Consulting Included</p> 
+      <p className="text-[#974] my-2">At the moment, I am taking appointments on Sundays and Monday evenings.</p>
+      <p className="text-[#974] my-2">My studio is out of my home in the Renton Highlands, I have made it a very cozy and peaceful space to share.</p>
+      <p className="text-[#974] my-2">I recommend comfortable, non bulky clothing but anything will do. You will not need to undress.</p>
+
+
+      <p className="text-2xl my-6  ">~What to Expect~</p> 
+      <p className="text-[#974] my-2">Sessions will start with a general check in, this can be quick or extensive. We will use our time depending on what is needed.</p>
+      <p className="text-[#974] my-2">During Table Time I let the energy guide me, scanning the Chakras and flowing energy to those that are in need of attention. I focus on ailments in all planes; mental, emotional, physical, spiritual and will bring in Crystals and Vibrational Sound to enhance healing if it feels right. This time will generally be very relaxing and mind clearing.</p>
+      <p className="text-[#974] my-2">We will end the session with another check in. I will let you know if I felt a strong pull in specific Chakras and can recommend Mindfulness tips on how to continue your healing outside of sessions. Mending inner and outer pain is a lifelong journey and centered around consistency so any other way I can assist, I will try my best to share.</p>
+      <p className="text-[#974] my-2">To schedule a session with me, please send me a message of your preferred time(s) and I will get back to you with my availability.</p>
+      <p className="text-[#974] my-2">I am looking forward to connecting {":)"}</p>
+
+
+      <p className="text-2xl my-6 ">~CONTACT~</p> 
+      <p className="text-[#974] my-">Text if you have my number.</p>
+      <p className="text-[#974] my-">yellowflowerenergy@gmail.com</p>
+      <p className="text-[#974] mb-20">@yellowflowerenergy</p>
+      {/* <p className="mt-5"></p> */}
+    </div>
     </div>
   );
 };
