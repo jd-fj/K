88f9ea4563fff558a4c88e30ee561bf552e233fe
jd-fj <61722743+jd-fj@users.noreply.github.com>
2023-10-05 20:50:43 -0700

    save progress

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,6 +2,7 @@ import { FunctionalComponent } from "preact";
 import { Link } from "preact-router/match";
 import butterfly from "../../assets/butterfly.png";
 import flowers from "../../assets/flowers.png";
+import './NavBar.css'
 
 const navLinks = [
   ["Reiki", "/reiki"],
@@ -16,41 +17,48 @@ const NavBar: FunctionalComponent = () => {
     <div className="flex flex-col items-stretch">
       {/* Top Horizontal Line */}
       <div className="flex items-center justify-between ">
-  <div className="w-28 h-auto md:w-36 mt-2 ">
-    <img src={butterfly} class="ml-1" alt="butterfly" />
-  </div>
+        <div className="w-28 h-auto md:w-36 mt-2 ">
+          <img src={butterfly} class="ml-1" alt="butterfly" />
+        </div>
 
-  <Link id="homeLink" href="/" className=""> {/* Adjust the mt-4 value as needed */}
-    <div className="sm:hidden">
-      <h2 className="text-orange-300 text-[2em] text-center">YELLOW FLOWER</h2>
-      <h2 className="text-orange-300 text-[2em] text-center">HOLISTIC HEALING CENTER</h2>
-    </div>
-    <div className="hidden sm:block">
-      <h2 className="text-orange-300 text-[2em] text-center">
-        YELLOW FLOWER HOLISTIC HEALING CENTER
-      </h2>
-    </div>
-  </Link>
+        <Link id="homeLink" href="/" className="">
+          {" "}
+          {/* Adjust the mt-4 value as needed */}
+          <div className="sm:hidden">
+            <h2 className="text-orange-300 text-[2em] text-center">
+              YELLOW FLOWER
+            </h2>
+            <h2 className="text-orange-300 text-[2em] text-center">
+              HOLISTIC HEALING CENTER
+            </h2>
+          </div>
+          <div className="hidden sm:block">
+            <h2 className="text-orange-300 text-[2em] text-center">
+              YELLOW FLOWER HOLISTIC HEALING CENTER
+            </h2>
+          </div>
+        </Link>
 
-  <div className="w-28 h-auto md:w-36 mt-2 mr-2">
-    <img src={flowers} alt="flowers" className=''/>
-  </div>
-</div>
+        <div className="w-28 h-auto md:w-36 mt-2 mr-2">
+          <img src={flowers} alt="flowers" className="" />
+        </div>
+      </div>
 
-
-
-      {/* Middle Horizontal Line */}
-      <nav id="NavLinks" className="flex justify-between mb-4">
-        {navLinks.map(([title, url]) => (
-          <Link
+{/* Middle Horizontal Line */}
+<nav id="NavLinks" className="flex justify-between mb-4">
+    {navLinks.map(([title, url]) => (
+        <Link
             key={title}
-            className="flex-1 text-center text-green-200 hover:text-green-100 hover:bg-green-200 rounded-lg responsive-text"
+            className="link-hover flex-1 text-center responsive-text"
             href={url}
-          >
-            {title}
-          </Link>
-        ))}
-      </nav>
+        >
+            <div className="link-child inline-block px-2 py-1 text-green-200 rounded-lg">
+                {title}
+            </div>
+        </Link>
+    ))}
+</nav>
+
 
       {/* Bottom Horizontal Line */}
       <div className="w-full">
