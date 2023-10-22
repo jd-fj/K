bc5ed534af039afb9538adc0eb4667f12cc06c7c
jd-fj <61722743+jd-fj@users.noreply.github.com>
2023-10-22 14:43:32 -0700

    save new files, preserve animation

--- src/components/Home/YellowFlower.tsx
+++ src/components/Home/YellowFlower.tsx
@@ -1,10 +1,14 @@
 // YellowFlower.tsx
 import { FunctionalComponent } from "preact";
+import SwirlyHand from "../../assets/SwirlyHand";
+import './YellowFlower.css'
 
 const YellowFlower: FunctionalComponent = () => {
   return (
-    <div id="homepage" className="">
-      homepage
+    <div className="h-1/4 w-1/4">
+      <div id="homepage" className="">
+        <SwirlyHand/>
+      </div>
     </div>
   );
 };
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -13,27 +13,27 @@ const navLinks = [
 const NavBar: FunctionalComponent = () => {
   return (
     <>
-      <Link
-        href="/"
-        id="HomeLink"
-        className="flex mx-2 text-center"
-      >
+      <Link href="/" id="HomeLink" className="flex mx-2 text-center xl:mx-[400px] lg:mx-[200px] md:mx-[100px] mx-auto -mb-5">
         <Full />
       </Link>
 
-      {/* Navigation Links */}
-      <div id="NavLinks" className="flex justify-center mb-1 w-full">
-        {navLinks.map(([title, url]) => (
-          <Link
+{/* Navigation Links */}
+<div
+    id="NavLinks"
+    className="flex h-[50px] justify-center md:space-x-[5em] sm:mb-1 -mb-1 w-full items-center " // added h-[50px] and items-center
+>
+    {navLinks.map(([title, url]) => (
+        <Link
             key={title}
-            className="flex link-hover justify-center text-center responsive-text flex-grow md:flex-grow-0 min-w-0"
+            className="flex items-center justify-center link-hover responsive-text flex-grow md:flex-grow-0 min-w-0 hover:bg-green-100 rounded-lg px-2 "
             href={url}
-          >
-            <div className="flex-shrink m-2 text-green-200 rounded-lg truncate text-center">
-              {title}
+        >
+            <div className="text-green-200 truncate text-center -mt-1">
+                {title}
             </div>
-          </Link>
-        ))}
+        </Link>
+    ))}
+
       </div>
       {/* Bottom Horizontal Row */}
       <div className="w-full ">
