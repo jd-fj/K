28c0332eacda29d08f461a878a2a9cb873ae7eba
jd-fj <61722743+jd-fj@users.noreply.github.com>
2023-12-09 15:18:50 -0800

    add margin top mobile hand

--- src/components/Home/YellowFlower.tsx
+++ src/components/Home/YellowFlower.tsx
@@ -6,7 +6,7 @@ import "./YellowFlower.css";
 const YellowFlower: FunctionalComponent = () => {
   return (
     <div className="flex justify-center m-5 ">
-      <div id="homepage" className="sm:h-1/4 sm:w-1/4 h-3/4 w-3/4 mr-4">
+      <div id="homepage" className="sm:h-1/4 sm:w-1/4 h-3/4 w-3/4 mr-4 mt-10 lg:mt-0">
         <SwirlyHand />
       </div>
     </div>
