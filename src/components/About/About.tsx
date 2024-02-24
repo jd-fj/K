1e804623c6cd70bf89609eb6cf7006570136b069
jd-fj <61722743+jd-fj@users.noreply.github.com>
2024-02-23 18:12:42 -0800

    add copy to about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,3 @@
-// About.tsx
 import { FunctionComponent } from "preact";
 import about from "/src/assets/about2_not_square.jpg";
 import Buddah from "../../assets/Buddah";
@@ -12,51 +11,68 @@ const About: FunctionComponent = () => {
       >
         <div
           id="aboutText"
-          className="flex-1 md:ml-[5%] lg:ml-[20%] text-center md:mx-0 mx-6 "
+          className="flex-1 md:ml-[5%] lg:ml-[20%] text-center md:mx-2 mx-6 "
         >
-          <p className="mb-4 -mt-4 lg:text-lg text-lg">Hello & thank you for visiting!</p>
+          <p className="mb-4 -mt-4 text-lg">Hello & thank you for visiting!</p>
 
-          <p className="mb-4 lg:text-lg text-lg">
+          <p className="mb-4 text-lg">
             My name is Kalee and I am truly blissed to be able to share with you
             the warmth and healing of Reiki energy and Mindfulness tools that
             can enhance compassion for the Self and others. Throughout my
             journey, I have delved into, practiced & healed from ~
           </p>
 
-          <p className="-mb-1 lg:text-lg text-lg">Reiki</p>
-          <p className="-mb-1 lg:text-lg text-lg">Chakra Work</p>
-          <p className="-mb-1 lg:text-lg text-lg">Hatha Yoga</p>
-          <p className="-mb-1 lg:text-lg text-lg">Buddhism</p>
-          <p className="-mb-1 lg:text-lg text-lg">Breathwork</p>
-          <p className="-mb-1 lg:text-lg text-lg">Tai Chi</p>
-          <p className="-mb-1 lg:text-lg text-lg">Mindfulness Coaching</p>
-          <p className="-mb-1 lg:text-lg text-lg">Internal Family Systems</p>
-          <p className="-mb-1 lg:text-lg text-lg">Nonviolent Communication</p>
-          <p className="-mb-1 lg:text-lg text-lg">Crystal Connection</p>
-          <p className="mb-4 lg:text-lg text-lg">Meditation</p>
+          <p className="-mb-1 text-lg">Reiki</p>
+          <p className="-mb-1 text-lg">Chakra Work</p>
+          <p className="-mb-1 text-lg">Hatha Yoga</p>
+          <p className="-mb-1 text-lg">Buddhism</p>
+          <p className="-mb-1 text-lg">Breathwork</p>
+          <p className="-mb-1 text-lg">Tai Chi</p>
+          <p className="-mb-1 text-lg">Mindfulness Coaching</p>
+          <p className="-mb-1 text-lg">Internal Family Systems</p>
+          <p className="-mb-1 text-lg">Nonviolent Communication</p>
+          <p className="-mb-1 text-lg">Crystal Connection</p>
+          <p className="mb-4 text-lg">Meditation</p>
 
-          <p className="mb-4 lg:text-lg text-lg">
-            I feel very grateful for the wisdom passed down from my Therapists &
-            Spiritual Teachers, and am very happy to be participating in the teachings and healing of others. Please reach out to connect.
+          <p className="mb-4 text-lg">
+            I feel very grateful for the wisdom passed down from my
+            Spiritual Teachers and am happy to be participating in the teachings and healing of others.
           </p>
-          <p className="lg:text-lg text-lg">Please reach out to connect.</p>
+          <p className="text-lg">Please reach out to connect.</p>
 
-          <p className="lg:text-lg text-lg">Namaste</p>
+          <p className="text-lg">Namaste</p>
+
+          {/* Desktop view Buddah component, visible only on md screens and up */}
+          <div className="hidden md:block ml-[58%]">
+            <Buddah className="h-10v w-10v" />
+          </div>
         </div>
 
         <div
           id="aboutImage"
-          className="flex justify-center items-start flex-1 mb-5 mt-5 md:mr-[18%] sm:mt-5 overflow-hidden  mx-10 md:mx-0 mx-auto w-1/2"
+          className="flex flex-col justify-center items-center flex-1 mb-5 mt-5 md:mr-[18%] sm:-mt-6 overflow-hidden mx-10 md:mx-0 mx-auto w-1/2"
         >
           <img
             src={about}
             alt="reiki practitioner pleasantly standing in the daylight"
-            className="sm:-mx-2.5 object-contain max-w-full flex-shrink-0 mb-4"
+            className="sm:-mx-0 object-contain  mb-4"
           />
+          <div id="newTextBlock" className="hidden md:block text-center text-lg mt-4">
+            <p className="-mb-1">Yoga Teacher Training </p>
+            <p className="mb-4">Hatha Yoga Center of Seattle / Bali</p>
+
+            <p className="-mb-1">Reiki Master Certification</p>
+            <p className="mb-4">Whidbey Island Reiki</p>
+
+            <p className="-mb-1">ACTP ICF Level 1 Coach (ACC)</p>
+            <p className="">Mindfulness Coaching School</p>
+          </div>
         </div>
       </div>
-      <div className="flex justify-end mr-[33%] sm:mr-[58%] mb-16">
-        <Buddah className="sm:h-10v sm:w-10v h-14v w-14v"/>
+
+      {/* Mobile view Buddah component, hidden on md screens and up */}
+      <div className="flex justify-end mr-[33%] mb-16 md:hidden">
+        <Buddah className="h-14v w-14v" />
       </div>
     </>
   );
