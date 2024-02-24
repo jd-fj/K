952ab812117bab072cb72e73e6476b81dcf00d9d
jd-fj <61722743+jd-fj@users.noreply.github.com>
2024-02-23 18:41:42 -0800

    change copy Sessions

--- src/components/Sessions/Sessions.tsx
+++ src/components/Sessions/Sessions.tsx
@@ -15,11 +15,11 @@ const Sessions: FunctionComponent = () => {
 
         {/* COLUMN 1 */}
         <div id="Column1" className="lg:w-1/4 flex lg:flex-col justify-end">
-          <img className="hide-on-mobile ml-auto hidden lg:block" src={TwoDaisies} />
+          <img className="hide-on-mobile ml-auto hidden " src={TwoDaisies} />
         </div>
 
         {/* COLUMN 2 */}
-        <div id="Column2" className="lg:w-1/3 mx-1">
+        <div id="Column2" className="lg:w-1/3 mx-1 mb-4">
           <h1 className="text-shadow-effect text-3xl lg:mb-4">
             <b className="mb-1 lg:mb-4">Sessions</b>
           </h1>
@@ -27,27 +27,38 @@ const Sessions: FunctionComponent = () => {
           <h2 className="text-2xl text-left">
             <b>Reiki Healing</b>
           </h2>
-          <p className="mx-6 leading-tight lg:text-lg text-lg">
+          <p className="mx-6 leading-tight text-lg">
             Hands on Reiki Healing Sessions with a focus on Mindfulness, the
             Physical Body & Chakra Alignment.
           </p>
-          <p className="text-xl lg:text-2xl mt-2">60 minute $120</p>
-          <p className="text-xl lg:text-2xl mb-4">90 minute $165</p>
+          <p className="text-xl lg:text-2xl mt-2">$120</p>
+          <p className="mx-6 leading-tight text-lg -mt-2 mb-6">(first session half price)</p>
+
+          <h2 className="text-2xl text-left">
+            <b>Reiki Healing , 5 Session Series</b>
+          </h2>
+          <p className="mx-6 leading-tight text-lg">
+            Offered for those who are interested in releasing blocked energy that may be stubborn or older in nature. Best if all 5
+            sessions are used within 30 days, valid through 90 days.
+          </p>
+          <p className="text-xl lg:text-2xl mt-2 mb-6">$500</p>
+          <h2 className="text-2xl text-left">
+            <b>Pet Reiki Healing</b>
+          </h2>
+          <p className="mx-6 leading-tight text-lg">
+            I travel to you and perform in a space the animal feels safe and comfortable in. Typically 20-30 minutes.
+          </p>
+          <p className="text-xl lg:text-2xl mt-2 mb-6">$60</p>
+
 
           <h2 className="text-2xl text-left">
             <b>Mindfulness Coaching</b>
           </h2>
-          <p className="mx-6 leading-tight lg:text-lg text-lg">
-            One on One or Partner Sessions that may incorporate Breathwork,
-            Meditation, Yoga Postures, learnings on Nonviolent Communication,
-            the Chakra System & Crystal Connection.</p>
-          <p className="text-xl lg:text-2xl mt-2 mb-4 lg:mb-8">60 minute $90</p>
-          <p className="mx-6 mb-2 leading-tight lg:text-lg text-lg">
-            Sessions are held out of my home in the Renton Highlands.
+          <p className="mx-6 leading-tight text-lg">
+            One on one talk sessions that are designed to inspire, invoke connection with the Self & move forward through life in a holistic way. May incorporate Breathwork, Meditation, learnings on Non-Violent Communication, the Chakra System & Crystal Connection.
           </p>
-          <p className="mb-4 mx-6 leading-tight lg:text-lg text-lg">
-            To schedule, please send me a message {"☺︎"}
-          </p>
+          <p className="text-xl lg:text-2xl mt-2 ">60 minute <span className="ml-4">$90</span></p>
+          <p className="mx-6 leading-tight text-lg -mt-2 mb-6">(Remote or in person available)</p>
         </div>
 
         {/* COLUMN 3 */}
@@ -55,11 +66,14 @@ const Sessions: FunctionComponent = () => {
           <h1 className="text-shadow-effect text-3xl mb-1 lg:mb-6">
             <b>Contact</b>
           </h1>
-          <p className="mx-6 leading-tight lg:text-lg text-lg">Text if you have my number.</p>
-          <p className="mx-6 leading-tight lg:text-lg text-lg">yellowflowerholistichealing@gmail.com</p>
-          <p className="mx-6 mb-4 leading-tight lg:text-lg text-lg">@yellowflowerholistichealing</p>
+          <p className="mx-6 mb-6 leading-tight text-lg">
+            Sessions are held out of my home in the Renton Highlands. To schedule, please send me a message {"☺︎"}
+          </p>
+          <p className="mx-6 leading-tight text-lg">Text if you have my number.</p>
+          <p className="mx-6 leading-tight text-lg">yellowflowerholistichealing@gmail.com</p>
+          <p className="mx-6 mb-4 leading-tight text-lg mb-12">@yellowflowerholistichealing</p>
           <img className="hide-on-desktop mx-auto mb-16 block lg:hidden" src={TwoDaisies} />
-          <img className="hide-on-mobile mx-auto hidden lg:block w-[14em] mt-12" src={LucyOutside} />
+          <img className="hide-on-mobile mx-auto hidden lg:block w-[16em] mt-12" src={LucyOutside} />
         </div>
 
 
